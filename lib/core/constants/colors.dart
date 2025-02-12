c8452e0dcd67a335614fd6e80c23ebafb274b476
Dodje <nostyy30@gmail.com>
2025-02-13 01:32:24 +0200

    colors

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,3 +1,11 @@
+import 'dart:ui';
+
 class AppColors{
+  static const primary = Color(0xFFB00010);
+  static const backGround = Color(0xFF002151);
+  static const mainBackGround = Color(0xFFE0E0E0);
+
+  static const white = Color(0xFF002131);
+
 
 }
